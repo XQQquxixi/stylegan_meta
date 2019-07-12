027c4ccf3129b72ebe7366c81dc83afc098e0cfc
xuqiqi <frances.qiqi.xu@gmail.com>
2019-07-12 15:16:25 -0400

    h

--- train.py
+++ train.py
@@ -1,26 +1,30 @@
 import os
 import argparse
-import tensorflow as tf
-from encoder.perceptual_model import LossModel
 import pickle
+from tqdm import tqdm
+import PIL.Image
+import numpy as np
 import dnnlib
 import dnnlib.tflib as tflib
 import config
-import PIL.Image
-import numpy as np
-from encoder.generatorModel import Generator
-from tqdm import tqdm
+from encoder.generator_model import Generator
+from encoder.perceptual_model import PerceptualModel
+import tensorflow as tf
+from scipy.stats import truncnorm
+
+URL_FFHQ = 'https://drive.google.com/uc?id=1MEGjdvVpUsu1jB4zrXZN7Y4kBBOzizDQ'  # karras2019stylegan-ffhq-1024x1024.pkl
 
 
 def split_to_batches(l, n):
     for i in range(0, len(l), n):
         yield l[i:i + n]
 
+
 def main():
     parser = argparse.ArgumentParser(description='Find latent representation of reference images using perceptual loss')
-    #parser.add_argument('src_dir', default='./data', help='Directory with images for encoding')
-    #parser.add_argument('generated_images_dir', default='./gen', help='Directory for storing generated images')
-    #parser.add_argument('dlatent_dir', default='./dgen', help='Directory for storing dlatent representations')
+    parser.add_argument('src_dir', help='Directory with images for encoding')
+    parser.add_argument('generated_images_dir', help='Directory for storing generated images')
+    parser.add_argument('dlatent_dir', help='Directory for storing dlatent representations')
     
     # for now it's unclear if larger batch leads to better performance/quality
     parser.add_argument('--batch_size', default=1, help='Batch size for generator and perceptual model', type=int)
@@ -34,25 +38,23 @@ def main():
     parser.add_argument('--randomize_noise', default=False, help='Add noise to dlatents during optimization', type=bool)
     args, other_args = parser.parse_known_args()
     
-    # ----------------------------------------------------------------------
-    ref_images = [os.path.join('./data/anime', x) for x in os.listdir('./data/anime')]
+    ref_images = [os.path.join(args.src_dir, x) for x in os.listdir(args.src_dir)]
     ref_images = list(filter(os.path.isfile, ref_images))
     
     if len(ref_images) == 0:
-        raise Exception('%s is empty' % './data/anime')
-    
-    os.makedirs('./gen', exist_ok=True)
-    os.makedirs('./dgen', exist_ok=True)
+        raise Exception('%s is empty' % args.src_dir)
 
-# ----------------------------------------------------------------------
+    os.makedirs(args.generated_images_dir, exist_ok=True)
+    os.makedirs(args.dlatent_dir, exist_ok=True)
+
 # Initialize generator and perceptual model
 tflib.init_tf()
-    with dnnlib.util.open_url('https://drive.google.com/uc?id=1MEGjdvVpUsu1jB4zrXZN7Y4kBBOzizDQ', cache_dir=config.cache_dir) as f:
+    with dnnlib.util.open_url(URL_FFHQ, cache_dir=config.cache_dir) as f:
         generator_network, discriminator_network, Gs_network = pickle.load(f)
 
 generator = Generator(Gs_network, args.batch_size, randomize_noise=args.randomize_noise)
-loss_model = LossModel(args.image_size, layer=9, batch_size=args.batch_size)
-# loss_model.build_loss_model()
+perceptual_model = PerceptualModel(args.image_size, layer=9, batch_size=args.batch_size)
+perceptual_model.build_perceptual_model(generator.generated_image)
 
 # Optimize (only) dlatents by minimizing perceptual loss between reference and generated images in feature space
 # vars
@@ -62,125 +64,28 @@ variable = []
         if "StyleMod" in var.name or 'learnable_dlatents' in var.name:
             variable.append(var)
 
-for images_batch in tqdm(split_to_batches(ref_images, args.batch_size), total=len(ref_images) // args.batch_size):
+for images_batch in tqdm(split_to_batches(ref_images, args.batch_size), total=len(ref_images)//args.batch_size):
     names = [os.path.splitext(os.path.basename(x))[0] for x in images_batch]
     
-    # Generate images from found dlatents and save them
-    generated_images = generator.generate_images()
-        op = loss_model.optimize(generated_images, images_batch, variable, iterations=args.iterations, learning_rate=args.lr)
-        
+    perceptual_model.set_reference_images(images_batch)
+        op = perceptual_model.optimize(variable, iterations=args.iterations, learning_rate=args.lr)
         pbar = tqdm(op, leave=False, total=args.iterations)
         for loss in pbar:
             pbar.set_description(' '.join(names)+' Loss: %.2f' % loss)
-        print(' '.join(names), ' loss:', loss)
-        
-        # reconstruction ./gen
-        generated_images = generator.generate_images()
-        # random ./dgen
-        random_mages = generator.generate_images(tf.random_normal(generator.dlatent_variable.shape))
-        
-        # generated_dlatents = generator.get_dlatents()
-        for rec, ran, img_name in zip(generated_images, random_mages, names):
-            rec_img = PIL.Image.fromarray(rec, 'RGB')
-            ran_img = PIL.Image.fromarray(ran, 'RGB')
-            rec_img.save(os.path.join('./gen', f'{img_name}.png'), 'PNG')
-            ran_img.save(os.path.join('./dgen', f'{img_name}.png'), 'PNG')
+    # Generate images from found dlatents and save them
+    generated_images = generator.generate_images()
+        generated_dlatents = generator.get_dlatents()
+        rand_img = generator.generate_images(truncnorm(-0.4, 0.4).rvs(18 * 512).astype("float32").reshape(1, 18, 512))
+        generator.set_dlatents(generated_dlatents)
+        for img_array, r_img, img_name in zip(generated_images, rand_img, names):
+            img = PIL.Image.fromarray(img_array, 'RGB')
+            img.save(os.path.join(args.generated_images_dir, f'{img_name}.png'), 'PNG')
+            
+            img = PIL.Image.fromarray(r_img, 'RGB')
+            img.save(os.path.join(args.dlatent_dir, f'{img_name}.png'), 'PNG')
 
-# generator.reset_dlatents()
+generator.reset_dlatents()
 
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-"""
-    import os
-    import argparse
-    import tensorflow as tf
-    from encoder.perceptual_model import LossModel
-    import pickle
-    import dnnlib
-    import dnnlib.tflib as tflib
-    import config
-    import PIL.Image
-    import numpy as np
-    from encoder.generator_model import Generator
-    from tqdm import tqdm
-    
-    
-    def split_to_batches(l, n):
-    for i in range(0, len(l), n):
-    yield l[i:i + n]
-    
-    
-    def main():
-    parser = argparse.ArgumentParser(description='Find latent representation of reference images using perceptual loss')
-    #parser.add_argument('src_dir', default='./data', help='Directory with images for encoding')
-    #parser.add_argument('generated_images_dir', default='./gen', help='Directory for storing generated images')
-    #parser.add_argument('dlatent_dir', default='./dgen', help='Directory for storing dlatent representations')
-    
-    # for now it's unclear if larger batch leads to better performance/quality
-    parser.add_argument('--batch_size', default=1, help='Batch size for generator and perceptual model', type=int)
-    
-    # Perceptual model params
-    parser.add_argument('--image_size', default=1024, help='Size of images for perceptual model', type=int)
-    parser.add_argument('--lr', default=1., help='Learning rate for perceptual model', type=float)
-    parser.add_argument('--iterations', default=1000, help='Number of optimization steps for each batch', type=int)
-    
-    # Generator params
-    parser.add_argument('--randomize_noise', default=False, help='Add noise to dlatents during optimization', type=bool)
-    args, other_args = parser.parse_known_args()
-    
-    # ----------------------------------------------------------------------
-    ref_images = [os.path.join('data/anime/', x) for x in os.listdir('data/anime/')]
-    ref_images = list(filter(os.path.isfile, ref_images))
-    
-    if len(ref_images) == 0:
-    raise Exception('%s is empty' % 'data/anime/')
-    print('dataset size: %d' % len(ref_images))
-    
-    os.makedirs('gen/', exist_ok=True)
-    os.makedirs('dgen/', exist_ok=True)
-    
-    # ----------------------------------------------------------------------
-    # Initialize generator and perceptual model
-    tflib.init_tf()
-    with dnnlib.util.open_url('https://drive.google.com/uc?id=1MEGjdvVpUsu1jB4zrXZN7Y4kBBOzizDQ', cache_dir=config.cache_dir) as f:
-    generator_network, discriminator_network, Gs_network = pickle.load(f)
-    
-    generator = Generator(Gs_network, args.batch_size, randomize_noise=args.randomize_noise)
-    loss_model = LossModel(args.image_size, layer=9, batch_size=args.batch_size)
-    loss_model.build_loss_model(generator.generated_image)
-    
-    # Optimize loss wrt variables
-    for images_batch in tqdm(split_to_batches(ref_images, args.batch_size), total=len(ref_images) // args.batch_size):
-    names = [os.path.splitext(os.path.basename(x))[0] for x in images_batch]
-    loss_model.set_reference_images(images_batch)
-    # latent variable
-    variable = [generator.dlatent_variable]
-    # batch sta
-    tvars = tf.trainable_variables()
-    print("==============")
-    for var in tvars:
-    if "StyleMod" in var.name:
-    variable.append(var)
-    if "learnable_dlatents" in var.name:
-    print(var.name)
-    print("==============")
-    op = loss_model.optimize(variable, iterations=args.iterations, learning_rate=args.lr)
-    # Generate images from found dlatents and save them
-    generated_images = generator.generate_images()
-    generated_dlatents = generator.get_dlatents()
-    for img_array, dlatent, img_name in zip(generated_images, generated_dlatents, names):
-    img = PIL.Image.fromarray(img_array, 'RGB')
-    # random
-    img.save(os.path.join('gen', f'{img_name}.png'), 'PNG')
-    # reconstruction
-    img.save(os.path.join('dgen', f'{img_name}.png'), 'PNG')
-    # interpolation
-    
-    generator.reset_dlatents()
-    """
